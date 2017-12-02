c17b2ad09630d4b079b5c75ed79af2377da1b728
rakor <freebsd@rakor-net.de>
2017-12-02 19:09:13 +0100

    resticcmd as shoutcut added

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # 
 # This script does nearly nothing.. But I am lazy
 
-SCRIPT=resticbackup
+SCRIPT="resticbackup resticcmd"
 TARGET=/usr/local/bin
 PERMISSIONS=555
 
@@ -11,14 +11,6 @@ SCRIPTDIR=`dirname $0`
 if [ `id -u` != "0" ]; then
 	echo "Must be run as root"
 	exit 1
-fi
-
-# Only do anything if the script has changed. 
-if cmp -s $SCRIPTDIR/$SCRIPT $TARGET/$SCRIPT ; then
-	echo "Files are identical. Nothing to do. Exiting"
-	exit 2
-else
-	echo "Installing $SCRIPT..."
 fi
 
 # determin the fileowner to set later
@@ -34,13 +26,15 @@ case  "$(uname -s)" in
 		echo "You should change the owner of the script"
 esac
 
-#copy the script
-cp $SCRIPTDIR/$SCRIPT $TARGET
-#change the permissions
-chmod $PERMISSIONS $TARGET/$SCRIPT
-#change the owner
-if [ -n "$OWNER" ]; then
-	chown $OWNER $TARGET/$SCRIPT
-fi
+for i in $SCRIPT ; do
+	#copy the script
+	cp $SCRIPTDIR/$i $TARGET
+	#change the permissions
+	chmod $PERMISSIONS $TARGET/$i
+	#change the owner
+	if [ -n "$OWNER" ]; then
+		chown $OWNER $TARGET/$i
+	fi
+	echo "Installed $i in $TARGET with owner $OWNER and permissions $PERMISSIONS."
+done
 
-echo "Installed in $TARGET/$SCRIPT with owner $OWNER and permissions $PERMISSIONS."
